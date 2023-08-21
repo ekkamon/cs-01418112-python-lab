6396c96555804860a47a71c599d9202efe2c73ac
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-08-21 15:31:34 +0700

    init: lab07 (ข้อ 1 - 3)

---
+++ lab07/1.py
@@ -0,0 +1,21 @@
+num = int(input('Enter a number: '))
+
+#pre-defined function:
+def change_to_abc(number):
+    result = ''
+    i = 0
+    
+    while i < number:
+        result += chr(ord('A') + i)
+        i += 1
+        
+    return result
+
+if num <= 0 or num > 26:
+    print('Invalid input, program terminates.')
+else:
+    i = 1
+    
+    while i <= num:
+        print(change_to_abc(i))
+        i += 1
