8749e9fabf049bfd776662d39ae3de22cccba560
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-08-21 23:00:03 +0700

    update: lab07 ข้อ 8

--- lab07/8.py
+++ lab07/8.py
@@ -1,28 +1,35 @@
-c = int(input())
-
+total = int(input())
 count = 0
 
-_min = ((2**32) - 1)
-_max = -1 * ((2**32) - 1)
+def check(a, b):
+    return a**2 + b**2 == total**2
 
-a = 1
+def find(start_scope, end_scope):
+    i = start_scope
+    while i <= end_scope:
+        x = start_scope
+        while x <= end_scope:
+            if check(i, x):
+                return i, x
+            x += 1
+        i += 1
+        
+    return False
 
-while a <= c-1:
-    b = 1
-    
-    while b <= c-1:
-        if (a ** 2) + (b ** 2) == c**2:
-            print(a, b)
-            if a < _min and b > _max:
-                print('asdasd, a, b')
-                if a < _min:
-                    _min = a
-                    
-                if b > _max:
-                    _max = b
-                    
-                count += 1
-        b += 1
-    a += 1
-    
+start = 1
+end = total - 1
+
+while True:
+    result = find(start, end)
+
+    if result == False:
+        break
+
+    a, b = result
+
+    count += 1
+
+    start = a + 1
+    end = b - 1
+
 print(count)
