2853475fd6a04f94aba5be376a3e3fb8c3ec97a0
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-10-09 13:38:13 +0700

    update!: lab13 ข้อ 1-3, 5-7

---
+++ lab13/1.py
@@ -0,0 +1,15 @@
+def namelist(names):
+  if len(names) == 0:
+    return ''
+  
+  if len(names) == 1:
+    return names[0]
+
+  start = [names[idx] for idx in range(len(names) -1)]
+  end = names[len(names)-1]
+
+  return f'{", ".join(start)} & {end}'
+
+print( namelist(['Bart', 'Viola', 'Peter', 'Nostel']) )
+print( namelist(['Bart', 'Viola']) )
+print( namelist(['Peter']) )
